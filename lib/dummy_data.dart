c1b055c4221d1a4692ad183cd4cf9606da09a128
reemelsaeed <reemelsaeed4@gmail.com>
2026-05-15 16:06:59 +0300

    message

---
+++ lib/dummy_data.dart
@@ -0,0 +1,19 @@
+import 'package:first_app/models/card_model.dart';
+
+List<CardModel> resturants = [
+  CardModel(
+    image: 'assets/images/f1.jpg',
+    sub_title: 'Burger - Chiken - Wings - Riches',
+    title: 'Rose Garden Resturant',
+  ),
+  CardModel(
+    image: 'assets/images/f2.jpg',
+    sub_title: 'Burger - Chiken - Wings - Riches',
+    title: 'Bazooka Resturant',
+  ),
+  CardModel(
+    image: 'assets/images/f3.jpg',
+    sub_title: 'Burger - Chiken - Wings - Riches',
+    title: 'Rose  Resturant',
+  ),
+];
---
+++ lib/models/card_model.dart
@@ -0,0 +1,11 @@
+class CardModel {
+  String image;
+  String title;
+  String sub_title;
+
+  CardModel({
+    required this.image,
+    required this.sub_title,
+    required this.title,
+  });
+}
